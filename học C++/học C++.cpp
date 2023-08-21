1de43bdc969873634717e3c297239a90d4448c47
NLMK1103 <ngleminhkhoa@gmail.com>
2023-08-21 16:04:10 +0700

    bai1

---
+++ học C++/học C++.cpp
@@ -0,0 +1,27 @@
+﻿// Let's begin your journey towards coding with the very first question of the coding world. Your task is to write code which prints Hello World.
+// Input: No Input
+// Output: Hello World
+// Your Task: In the function helloWorld(), output a line "Hello World" (without quotes).
+// ---------------------------------------------------------------------------------------------------
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+// } Driver Code Ends
+
+class Solution {
+public:
+    void helloWorld() {
+        std::cout << "HelloWorld" << std::endl;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    Solution ob;
+    //function call
+    ob.helloWorld();
+    cout << endl;
+    return 0;
+}
+// } Driver Code Ends
